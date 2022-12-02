b9ecc423e8808b5bcc352e99e46b0fab1c3cea66
ZekeAranyLucas <zeke.aranylucas@gmail.com>
2022-12-02 09:45:44 +0100

    published working site to zekearanylucas.github.io/tweets

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "ZekeAranyLucas", // No leading @ here
+	homeLabel: "Introspective Developer",
+	homeUrl: "https://blog.introspectivedeveloper.com/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
